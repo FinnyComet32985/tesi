6298eef0a53558c1265063b25a0889aeeb36cbfd
FinnyComet32985 <lorenzoresta03@gmail.com>
2025-12-11 16:24:42 +0100

    creazione delle api key e del base url

--- fetcher.py
+++ fetcher.py
@@ -1,5 +1,16 @@
 import requests
 import json
 import time
-import datetime
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+
+# get API KEY from .dotenv file 
+load_dotenv()
+API_KEY=os.getenv("API_KEY")
+
+# set the BASE URL for the http request
+BASE_URL="https://api.clashroyale.com/v1"
+
 
